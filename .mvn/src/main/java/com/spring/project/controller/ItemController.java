e42da15e30996682ef951cb99271732aa95eb037
Sova <sovadas704@gmail.com>
2026-02-10 22:54:21 +0530

    uploading full project

---
+++ .mvn/src/main/java/com/spring/project/controller/ItemController.java
@@ -0,0 +1,28 @@
+package com.spring.project.controller;
+
+import com.spring.project.model.Item;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    private List<Item> itemList= new ArrayList<>();
+    @PostMapping
+    public String addItem(@Valid@RequestBody Item item){
+        itemList.add(item);
+        return "Item added successfully!";
+    }
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable Integer id){
+        for (Item item : itemList){
+            if(item.getId().equals(id)){
+                return item;
+            }
+        }
+        throw new RuntimeException("Item not found with id:"+id);
+    }
+
+}
---
+++ .mvn/src/test/java/com/spring/project/ProjectSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
